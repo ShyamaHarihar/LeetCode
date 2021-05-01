56b77e291c4e7d72c9517e8d28c01b8c7d69cc5d
Shyama Harihar <59372076+ShyamaHarihar@users.noreply.github.com>
2021-05-01 08:16:09 +0530

    Added comments
    Comments have been added

--- remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -13,12 +13,15 @@ public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
     int size=0;
     ListNode* t=head;
+        //Calculate size of the List
         while(t)
         {
             t=t->next;
             size++;
         }
+        //If it is 2 from end then it is size-2 from beginning 
         int index=size-n;
+        //if the node to be deleted from the beginning has a position 0 that means return head->next which is a corner case
         if(index==0)
         {
             head=head->next;
@@ -28,12 +31,13 @@ public:
         ListNode* p=NULL;
         int i=0;
         t=head;
+        //Normal iteration storing previous node 
         while(i<index && t->next!=NULL)
         {
             p=t;
             t=t->next;
             i++;
-        }
+        }//Way to skip a node and connect them 
         p->next=p->next->next;
         return head;
     }
