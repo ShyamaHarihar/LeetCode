12e61ab81ed59f7652a5a5d924b41615f6d2ae53
Shyama Harihar <59372076+ShyamaHarihar@users.noreply.github.com>
2021-07-20 18:31:05 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (99.66%) - LeetHub

--- pascals-triangle/pascals-triangle.cpp
+++ pascals-triangle/pascals-triangle.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
+    //Create a vector of vectors to store the answers so that we can access previous answers
     vector<vector<int>> res;
-        for(int i=0;i<numRows;i++)
+    //Now let us start creating each row
+    for(int i=0;i<numRows;i++)
+    {
+        //The number of elements in each row increase by 1 and lets initialise all to 1 
+        vector<int> row(i+1,1);
+        for(int j=1;j<i;j++)
         {
-            vector<int> row(i+1,1);
-            for(int j=1;j<i;j++)
-            {
-               row[j]=res[i-1][j]+res[i-1][j-1]; 
-            }
-            res.push_back(row);
+            //add two values of previous row just above as shown in question
+            row[j]=res[i-1][j]+res[i-1][j-1];
         }
+        res.push_back(row);//add the row to result vector
+    }
         return res;
     }
 };
