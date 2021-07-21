80c240b98db807b5108e5ddf29fa5be7081ec413
Shyama Harihar <59372076+ShyamaHarihar@users.noreply.github.com>
2021-07-21 10:58:36 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (18.46%) - LeetHub

--- middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -17,13 +17,11 @@ public:
         {
             return head;
         }
-        //Here head should not be NULL and head->next is NULL then that means one element in the list 
         while(f && f->next)
         {
-            s=s->next;
             f=f->next->next;
+            s=s->next;
         }
-        //Basically Two Pointer technique is used but the main thing here is the while loop condition -- fast->next should not be NULL as that shows we reached end of the iteration and fast should also not be NULL that means we crossed the limit 
         return s;
     }
 };
