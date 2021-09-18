97ab26720c0628c79d3acf098cd368f6c2fc4dde
Shyama Harihar <59372076+ShyamaHarihar@users.noreply.github.com>
2021-09-18 10:58:21 +0530

    Time: 23 ms (39.83%), Space: 11.8 MB (16.99%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> res;
-    unordered_map<int,int> map;
-    for(int i=0;i<nums.size();i++)
-    {
-        if(map.find(target-nums[i])!=map.end())
+       unordered_map<int,int> map;
+        vector<int> res;
+        for(int i=0;i<nums.size();i++)
         {
-            res.push_back(i);
-            res.push_back(map[target-nums[i]]);
-        }
-        else
-        {
-            map[nums[i]]=i;
-        }
+            if(map.find(target-nums[i])!=map.end())
+            {
+                res.push_back(i);//in the hashmap target-num[i] exists meaning there is a pair
+                //so we need to store them in the hashmap
+                res.push_back(map[target-nums[i]]);
+            }
+            else
+            {
+                map[nums[i]]=i;//store element and index in the hasmap
+            }
         }
         return res;
     }
