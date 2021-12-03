5bdcca730bae0bc7799b9fbc777d9b383f558472
Shyama Harihar <59372076+ShyamaHarihar@users.noreply.github.com>
2021-12-03 19:39:47 +0530

    Time: 28 ms (59.58%), Space: 27.4 MB (65.55%) - LeetHub

--- add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
+++ add-to-array-form-of-integer/add-to-array-form-of-integer.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
     for(int i=num.size()-1;i>=0 && k>0;i--)
     {
-        num[i]=num[i]+k;
-        k=num[i]/10;
-        num[i]=num[i]%10;
+       num[i]=num[i]+k;//how we add numbers from the end
+       k=num[i]/10;//the carry part
+       num[i]=num[i]%10;//the remainder part
     }
         while(k>0)
         {
@@ -13,6 +13,6 @@ public:
             k=k/10;
         }
         return num;
+        
     }
 };
-//o(n) solution and o(1) space
