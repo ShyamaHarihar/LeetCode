ac818015bffd1368f4c60a953eb7f799ec41e3ef
Shyama Harihar <59372076+ShyamaHarihar@users.noreply.github.com>
2021-12-04 22:11:21 +0530

    Time: 9 ms (19.84%), Space: 9.7 MB (29.16%) - LeetHub

--- kth-missing-positive-number/kth-missing-positive-number.cpp
+++ kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
     int n=arr.size();
-        if(n==0)
+    if(n==0)
+    {
+        //that means no elements so kth missing number is k LOL
+        return k;
+    }
+    vector<bool> p(2005,false);//this is because arr.length is 1000 max and k is also 1000 max
+    for(int i=0;i<n;i++)
+    {
+       p[arr[i]]=true; 
+    }
+    int c=0;
+    int res=0;
+    for(int i=1;i<=2005;i++)
+    {
+        if(p[i]==false)
         {
-            return k;
-        }
-        vector<bool> precompute(2001,false);
-        for(int i=0;i<n;i++)
-        {
-            precompute[arr[i]]=true;
-        }
-        int c=0;
-        int res;
-        for(int i=1;i<=2000;i++)
-        {
-            if(precompute[i]==false)
+            c++;
+            if(c==k)
             {
-                c++;
-                if(c==k)
-                {
-                    res=i;
-                    break;
-                }
+                res=i;
+                break;
             }
         }
-        return res;
+    }
+    return res;
     }
 };
