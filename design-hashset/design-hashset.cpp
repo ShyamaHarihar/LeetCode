8db9d9ba05588daa5774ab1762a949d214830ad5
Shyama Harihar <59372076+ShyamaHarihar@users.noreply.github.com>
2021-12-22 10:29:54 +0530

    Time: 84 ms (76.61%), Space: 41.8 MB (59.10%) - LeetHub

--- design-hashset/design-hashset.cpp
+++ design-hashset/design-hashset.cpp
@@ -3,38 +3,39 @@ public:
     vector<list<int>> m;
     int size;
     MyHashSet() {
-    size=10;
+    size=1000;
     m.resize(size);
     }
-    /*hashing function just modulo to do chaining*/
     int hash(int key)
     {
         return key%size;
     }
-    /*search function*/
     list<int> :: iterator search(int key)
     {
-       int i=hash(key);//index of vector at which the element is present in the doubly linked list
-        //use find function of DLL from STL
+        int i=hash(key);
         return find(m[i].begin(),m[i].end(),key);
-       
     }
     void add(int key) {
-    if(contains(key)) return;
     int i=hash(key);
+    if(contains(key)) return;
     m[i].push_back(key);
     }
     
     void remove(int key) {
-        if(!contains(key)) return;
-        int i=hash(key);
+    int i=hash(key);
+    if(contains(key))
+    {
         m[i].erase(search(key));
+    }
     }
     
     bool contains(int key) {
     int i=hash(key);
-    if(search(key)!=m[i].end()) return true;
-    else return false;
+    if(search(key)!=m[i].end())
+    {
+        return true;
+    }
+        return false;
     }
 };
 
