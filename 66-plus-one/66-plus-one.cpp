59d07f3eddff5881bc5216fef44564e2a959d4bf
Shyama Harihar <59372076+ShyamaHarihar@users.noreply.github.com>
2022-03-29 18:48:18 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (12.90%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-    //case [9,9,9] converts to [1,0,0,0]
-    for(int i=digits.size()-1;i>=0;i--)
+    int n=digits.size()-1;
+    if(digits[n]!=9)
     {
-        if(digits[i]==9)
-        {
-            digits[i]=0;
-        }
-        else{
-            digits[i]+=1;
-            return digits;
-            //[1,2,3]->[1,2,4]
-        }
+        digits[n]++;
+        return digits;
     }
-        digits[0]=1;
-        digits.push_back(0);
+        else{
+            for(int i=n;i>=0;i--)
+            {
+                
+                if(digits[i]==9)
+                {
+                    digits[i]=0;
+                }
+                else{
+                    digits[i]+=1;
+                    return digits;
+                }
+            }
+            digits[0]=1;
+            digits.push_back(0);
+        }
         return digits;
     }
 };
