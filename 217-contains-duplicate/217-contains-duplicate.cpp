200bab3290413c5881866a127518c5171a108001
Shyama Harihar <59372076+ShyamaHarihar@users.noreply.github.com>
2022-08-17 11:25:25 +0530

    Time: 142 ms (58.15%), Space: 53.5 MB (20.55%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+    unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        for(auto it:mp)
+        {
+            if(it.second>=2)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
